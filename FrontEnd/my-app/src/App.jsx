ff431afed56fffba72f5218ba1214fc5cab200a5
Lim Jiexian <72352102+limjiexian@users.noreply.github.com>
2022-09-18 03:35:54 +0800

    added nav stuff
    added nav stuff

--- FrontEnd/my-app/src/App.jsx
+++ FrontEnd/my-app/src/App.jsx
@@ -1,6 +1,13 @@
 import Nav from "./components/Nav";
 
 export default function App() {
+  const nData = {
+    id: 1,
+    userName: "AnimatedFred",
+    displayPictureUrl: "https://i.imgflip.com/1g8my4.jpg",
+    email: "animated@demo.com",
+  };
+
   return (
     <>
       <Nav />
---
+++ FrontEnd/my-app/src/NavData.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    id: 1,
+    userName: "AnimatedFred",
+    displayPictureUrl: "https://i.imgflip.com/1g8my4.jpg",
+    email: "animated@demo.com",
+  },
+];
