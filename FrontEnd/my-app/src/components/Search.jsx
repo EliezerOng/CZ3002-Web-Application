a2babf5523227ef981c2b76dc0b499f54d76d69f
Lim Jiexian <72352102+limjiexian@users.noreply.github.com>
2022-10-10 02:26:23 +0800

    fixed css when using backend data
    fixed css when using backend data

--- FrontEnd/my-app/src/components/Search.jsx
+++ FrontEnd/my-app/src/components/Search.jsx
@@ -5,10 +5,12 @@ import "./Search.css";
 export default function Search() {
   return (
     <div className="search">
-      <div className="main-container">
-        <div className="container-1">
-          <input type="text" placeholder="search post..."></input>
-        </div>
+      <div className="container-1">
+        <input
+          type="text"
+          placeholder="search post..."
+          className="search-input"
+        ></input>
       </div>
     </div>
   );
